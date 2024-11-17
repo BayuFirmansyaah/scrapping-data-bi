341cdeca6adf844ec7b1c270f4ffb7054a5daa16
BayuFirmansyaah <bayufirmansyah2800@gmail.com>
2024-11-17 20:53:41 +0700

    (add) - condition when folder not exist

--- app.js
+++ app.js
@@ -67,6 +67,11 @@ const scrapper = async () => {
             const url = `https://www.bi.go.id/hargapangan/WebSite/TabelHarga/GetGridDataDaerah?price_type_id=1&comcat_id=cat_2%2Ccom_7%2Ccat_5%2Ccom_11%2Ccat_6%2Ccom_12%2Ccat_7%2Ccom_13%2Ccom_14%2Ccat_8%2Ccom_15%2Ccom_16&province_id=${keyProvinces[i]}&regency_id=&market_id=&tipe_laporan=1&start_date=${year}-01-01&end_date=${year}-12-31&_=1730856415111`;
     
             const data = await runAPI(url);
+
+            if(!fs.existsSync(`./data/${year}`)) {
+                fs.mkdirSync(`./data/${year}`);
+            }
+
             fs.writeFileSync(`./data/${year}/${valuesProvinces[i]}.json`, JSON.stringify(data, null, 2));
     
             countingTimes.push((Date.now() - timeStart)/1000);
